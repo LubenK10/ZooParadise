9a3c03171138a786dd349baf95da3b7b71de4ea6
Luben Kulishev <lubaka1020@outlook.com>
2022-12-05 16:52:36 +0200

    Implemented Manager Controller

--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZooParadise.Core.Constants;
+using ZooParadise.Core.Contracts;
+using ZooParadise.Core.Models.Manager;
+using ZooParadise.Extensions;
 
 namespace ZooParadise.Controllers
 {
+    [Authorize]
     public class ManagerController : Controller
     {
-        public IActionResult Index()
+        private readonly IManagerService managerService;
+
+        public ManagerController(IManagerService _managerService)
         {
-            return View();
+            this.managerService = _managerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BecomeManager()
+        {
+            if (await managerService.IdExist(User.Id()))
+            {
+                TempData[MessageConstants.ErrorMessage] = "You are already a Manager!";
+
+                return RedirectToAction("Index", "Home");
+            }
+            var model = new ManagerModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BecomePro(ManagerModel model)
+        {
+            var userId = User.Id();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await managerService.IdExist(userId))
+            {
+                TempData[MessageConstants.ErrorMessage] = "You are already a Manager!";
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (await managerService.PhoneExist(model.PhoneNumber))
+            {
+                TempData[MessageConstants.ErrorMessage] = "User with that telephone number already exist!";
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            await managerService.Create(userId, model.PhoneNumber);
+
+            TempData[MessageConstants.SuccessMessage] = "Congatulation! You are a Manager!";
+
+            return RedirectToAction("All", "Pet");
+
         }
     }
 }
