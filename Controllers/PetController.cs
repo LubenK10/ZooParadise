5af7ebdab8e95c7f818c132d261702a615f687d2
Luben Kulishev <lubaka1020@outlook.com>
2022-12-07 19:41:23 +0200

    Add Method implemented

--- Controllers/PetController.cs
+++ Controllers/PetController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZooParadise.Core.Contracts;
+using ZooParadise.Core.Models.Pet;
+using ZooParadise.Extensions;
 
 namespace ZooParadise.Controllers
 {
+    [Authorize]
     public class PetController : Controller
     {
-        public IActionResult Index()
+
+        private readonly IPetService petService;
+
+        private readonly IManagerService managerService;
+
+        private readonly ILogger logger;
+
+        public PetController(
+            IPetService _petService,
+            IManagerService _managerService,
+            ILogger<PetController> _logger)
         {
-            return View();
+            petService = _petService;
+            managerService = _managerService;
+            logger = _logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new AddPetModel()
+            {
+                AnimalTypes = await petService.AllAnimalTypes(),
+                Genders = await petService.AllGenders(),
+                Colours = await petService.AllColours()
+
+            };
+
+            return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Add(AddPetModel model)
+        {
+            
+            if (!ModelState.IsValid)
+            {
+                model.AnimalTypes = await petService.AllAnimalTypes();
+                model.Genders = await petService.AllGenders();
+                model.Colours = await petService.AllColours();
+
+                return View(model);
+            }
+
+            if (await managerService.IdExist(User.Id()) == false)
+            {
+                RedirectToAction("Become", "Agent");
+            }
+
+            var managerId = await managerService.TakeManagerId(User.Id());
+
+            int id = await petService.Create(model, managerId);
+
+            return RedirectToAction("Details", new { id });
         }
     }
 }
+
