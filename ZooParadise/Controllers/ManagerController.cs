a1aae35815a3d29f4ec59caaca9ea718b77f4d08
LubenK10 <104639860+LubenK10@users.noreply.github.com>
2022-12-11 22:43:32 +0200

    Add files via upload

---
+++ ZooParadise/Controllers/ManagerController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZooParadise.Core.Constants;
+using ZooParadise.Core.Contracts;
+using ZooParadise.Core.Models.ManagerModels;
+using ZooParadise.Extensions;
+
+namespace ZooParadise.Controllers
+{
+    [Authorize]
+    public class ManagerController : Controller
+    {
+        private readonly IManagerService managerService;
+
+        public ManagerController(IManagerService _managerService)
+        {
+            this.managerService = _managerService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Become()
+        {
+            if (await managerService.IdExist(User.Id()))
+            {
+                TempData[MessageConstants.ErrorMessage] = "You are already a Manager!";
+
+                return RedirectToAction("Index", "Home");
+            }
+            var model = new ManagerModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Become(ManagerModel model)
+        {
+            var userId = User.Id();
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (await managerService.IdExist(userId))
+            {
+                TempData[MessageConstants.ErrorMessage] = "You are already a Manager!";
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            if (await managerService.PhoneExist(model.PhoneNumber))
+            {
+                TempData[MessageConstants.ErrorMessage] = "User with that telephone number already exist!";
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            await managerService.Create(userId, model.PhoneNumber);
+
+            TempData[MessageConstants.SuccessMessage] = "Congatulation! You are a Manager!";
+
+            return RedirectToAction("All", "Pet");
+
+        }
+    }
+}
---
+++ ZooParadise/Extensions/ZooParadiseServiceCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using ZooParadise.Core.Contracts;
+using ZooParadise.Core.Services;
+using ZooParadise.Infrastructure.Data.Common;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ZooParadiseServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IPetService, PetService>();
+            services.AddScoped<IManagerService, ManagerService>();
+
+            return services;
+        }
+    }
+}
