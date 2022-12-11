14f1407f142909e4b47df8ae978b7db60a5970c6
LubenK10 <104639860+LubenK10@users.noreply.github.com>
2022-12-11 22:12:12 +0200

    Add files via upload

---
+++ ZooParadise.Core/Contracts/IManagerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooParadise.Core.Contracts
+{
+    public interface IManagerService
+    {
+        Task<bool> IdExist(string userId);
+
+        Task<bool> PhoneExist(string phoneNumber);
+
+        Task<int> TakeManagerId(string userId);
+
+        Task<bool> UserAdopts(string userId);
+        
+
+        Task Create(string userId, string phoneNumber);
+    }
+}
