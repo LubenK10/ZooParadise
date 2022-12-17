10bdf182c8038fa28e09811965cf8972a9fd3caf
Luben Kulishev <104639860+LubenK10@users.noreply.github.com>
2022-12-17 21:36:07 +0200

    Add files via upload

---
+++ ZooParadise/Extensions/ClaimsExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace ZooParadise.Extensions
+{
+    public static class ClaimsExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
---
+++ ZooParadise/Extensions/ZooParadiseServiceCollectionExtension.cs
@@ -0,0 +1,20 @@
+﻿using ZooParadise.Core.Contracts;
+using ZooParadise.Core.Contracts.Admin;
+using ZooParadise.Core.Services;
+using ZooParadise.Core.Services.Admin;
+using ZooParadise.Infrastructure.Data.Common;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ZooParadiseServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IPetService, PetService>();
+            services.AddScoped<IManagerService, ManagerService>();
+            services.AddScoped<IUserService, UserService>();
+            return services;
+        }
+    }
+}
