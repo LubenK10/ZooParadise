78647aa6b60175d55c66da4cf994dfaa8a2d927a
Luben Kulishev <104639860+LubenK10@users.noreply.github.com>
2022-12-18 10:37:31 +0200

    Add files via upload

---
+++ Mock/MapperMock.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooParadise.Tests.Mock
+{
+    public static class MapperMock
+    {
+       //public static IMapper Instance
+       //{
+       //    get
+       //    {
+       //        var mapperConfiguration = new MapperConfiguration(config =>
+       //        {
+       //            config.AddProfile<ServiceMappingProfile>();
+       //        });
+       //
+       //        return new Mapper(mapperConfiguration);
+       //    }
+       //}
+    }
+}
